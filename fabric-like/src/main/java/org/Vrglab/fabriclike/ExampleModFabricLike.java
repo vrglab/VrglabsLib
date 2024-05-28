eff163592f049001053b64404fe99473653de0fc
Vrglab <vrglab1@gmail.com>
2024-05-28 14:29:13 +0200

    Architechtory Quilt, Registrate needs to still be added to forge

---
+++ fabric-like/src/main/java/org/Vrglab/fabriclike/ExampleModFabricLike.java
@@ -0,0 +1,10 @@
+package org.Vrglab.fabriclike;
+
+import org.Vrglab.ExampleMod;
+
+public final class ExampleModFabricLike {
+    public static void init() {
+        // Run our common setup.
+        ExampleMod.init();
+    }
+}
