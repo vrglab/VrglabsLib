677d23b7458c8aaccb572e000f9793b40331fa73
Vrglab <vrglab1@gmail.com>
2024-08-29 10:09:30 +0200

    1.20.4: implemented CMT's into the auto-reg system

--- 1.20.4/common/src/main/java/org/TestSystem/world/Items/CreativeModeTabs.java
+++ 1.20.4/common/src/main/java/org/TestSystem/world/Items/CreativeModeTabs.java
@@ -3,22 +3,19 @@ package org.TestSystem.world.Items;
 import org.TestSystem.TestMod;
 import org.TestSystem.world.Blocks.TestBlocks;
 import org.Vrglab.AutoRegisteration.Annotations.InitializableClass;
+import org.Vrglab.AutoRegisteration.Annotations.RegisterCMT;
+import org.Vrglab.AutoRegisteration.Objects.RegistryCMT;
 import org.Vrglab.Helpers.CreativeModeTabBuilder;
 import org.Vrglab.Modloader.Registration.Registry;
 
 import java.util.ArrayList;
 
-@InitializableClass
 public class CreativeModeTabs {
 
-    public static Object TEST_GROUP = Registry.RegisterCreativeModeTab("test_tab", TestMod.MODID,
-            CreativeModeTabBuilder.create()
-                    .setDisplayText("test_tab")
-                    .setIcon(TestItems.TEST_ITEM.getRawData())
-                    .setEntries(TestItems.getAll(), TestBlocks.getAll())
-                    .build()
-    );
-
-    public static void init(){
-    }
+    @RegisterCMT(Name = "test_tab")
+    public static RegistryCMT TEST_GROUP = new RegistryCMT(TestMod.MODID,  CreativeModeTabBuilder.create()
+            .setDisplayText("test_tab")
+            .setIcon(TestItems.TEST_ITEM.getRawData())
+            .setEntries(TestItems.getAll(), TestBlocks.getAll())
+            .build());
 }
