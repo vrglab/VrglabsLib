b9c6af5aee8a13d0756f87d3f520b59375f97f6f
Vrglab <vrglab1@gmail.com>
2024-08-29 21:42:54 +0200

    1.20.4: Small Fixes

---
+++ 1.20.4/common/src/main/java/org/TestSystem/package-info.java
@@ -0,0 +1,4 @@
+@ApiStatus.Internal
+package org.TestSystem;
+
+import org.jetbrains.annotations.ApiStatus;
--- 1.20.4/common/src/main/java/org/Vrglab/Utils/VLModInfo.java
+++ 1.20.4/common/src/main/java/org/Vrglab/Utils/VLModInfo.java
@@ -1,10 +1,11 @@
 package org.Vrglab.Utils;
 
 import org.Vrglab.VrglabsLib;
+import org.jetbrains.annotations.ApiStatus;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-
+@ApiStatus.Internal
 public class VLModInfo {
     public static final String MOD_ID = "vrglabslib";
 
