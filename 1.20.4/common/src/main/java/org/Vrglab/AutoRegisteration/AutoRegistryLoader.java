e69238f06f2109e02cde140dc1f95fca1ff60151
Vrglab <vrglab1@gmail.com>
2024-09-04 08:40:57 +0200

    1.20.4: Clean ups

--- 1.20.4/common/src/main/java/org/Vrglab/AutoRegisteration/AutoRegistryLoader.java
+++ 1.20.4/common/src/main/java/org/Vrglab/AutoRegisteration/AutoRegistryLoader.java
@@ -25,50 +25,51 @@ public class AutoRegistryLoader {
     static ClassLoader loader = null;
 
     public static void LoadAllInPackage(String packageName, String modid) {
-        loadItemsInPackage(packageName, modid);
-        loadBlocksInPackage(packageName, modid);
-        loadItemlessBlocksInPackage(packageName, modid);
-        loadBlockEntityTypesInPackage(packageName, modid);
-        callInitsInPackage(packageName, modid);
-        loadCMTInPackage(packageName, modid);
-    }
 
-    public static void loadItemsInPackage(String packageName, String modId) {
-        LoadingResolver(packageName, modId, RegisterItem.class, RegistryItem.class, (rg, rt) -> {
-            Object return_val = Registry.RegisterItem(rt.ItemName(), modId, rg.getSupplier());
+        /** ITEMS **/
+        LoadingResolver(packageName, modid, RegisterItem.class, RegistryItem.class, (rg, rt) -> {
+            Object return_val = Registry.RegisterItem(rt.ItemName(), modid, rg.getSupplier());
             rg.setRegistryData(return_val);
             return return_val;
         });
-    }
 
-    public static void loadBlocksInPackage(String packageName, String modId) {
-        LoadingResolver(packageName, modId, RegisterBlock.class, RegistryBlock.class, (rg, rt) -> {
-            Object return_val = Registry.RegisterBlock(rt.Name(), modId, rg.getSupplier(), (Supplier<Item.Settings>)rg.getArgs().get("item.settings"));
+        /** BLOCKS **/
+        LoadingResolver(packageName, modid, RegisterBlock.class, RegistryBlock.class, (rg, rt) -> {
+            Object return_val = Registry.RegisterBlock(rt.Name(), modid, rg.getSupplier(), (Supplier<Item.Settings>)rg.getArgs().get("item.settings"));
             rg.setRegistryData(return_val);
             return return_val;
         });
-    }
 
-    public static void loadItemlessBlocksInPackage(String packageName, String modId) {
-        LoadingResolver(packageName, modId, RegisterItemlessBlock.class, RegistryItemlessBlock.class, (rg, rt) -> {
-            Object return_val = Registry.RegisterItemlessBlock(rt.Name(), modId, rg.getSupplier());
+        /** ITEMLESS BLOCKS **/
+        LoadingResolver(packageName, modid, RegisterItemlessBlock.class, RegistryItemlessBlock.class, (rg, rt) -> {
+            Object return_val = Registry.RegisterItemlessBlock(rt.Name(), modid, rg.getSupplier());
             rg.setRegistryData(return_val);
             return return_val;
         });
-    }
 
-    public static void loadBlockEntityTypesInPackage(String packageName, String modId) {
-        LoadingResolver(packageName, modId, RegisterBlockEntityType.class, RegistryBlockEntityType.class, (rg, rt) -> {
+        /** BLOCK ENTITIES **/
+        LoadingResolver(packageName, modid, RegisterBlockEntityType.class, RegistryBlockEntityType.class, (rg, rt) -> {
             Object return_val = Registry.RegisterBlockEntityType(
                     rt.Name(),
-                    modId,
+                    modid,
                     (IBlockEntityLoaderFunction) rg.getArgs().get("new"),
                     (rg.getArgs().get("block") instanceof RegistryBlock) ? (entityTypeBlockSelector.accept(rg)) : rg.getArgs().get("block")
             );
             rg.setRegistryData(return_val);
             return return_val;
         });
+
+        /** CLASS INIT FUNCTIONS **/
+        callInitsInPackage(packageName, modid);
+
+        /** CREATIVE MODE TABS **/
+        LoadingResolver(packageName, modid, RegisterCMT.class, RegistryCMT.class, (rg, rt) -> {
+            Object return_val = Registry.RegisterCreativeModeTab(rt.Name(), modid, (ItemGroup)rg.getRawData());
+            rg.setRegistryData(return_val);
+            return return_val;
+        });
     }
+
 
     private static void callInitsInPackage(String packageName, String modId) {
         Set<Class> annotatedType = getTypesAnnotatedIn(InitializableClass.class, packageName, modId);
@@ -81,14 +82,6 @@ public class AutoRegistryLoader {
             } catch (NoSuchMethodException e) {
                 VLModInfo.LOGGER.error(e.getMessage());
             }
-        });
-    }
-
-    public static void loadCMTInPackage(String packageName, String modId) {
-        LoadingResolver(packageName, modId, RegisterCMT.class, RegistryCMT.class, (rg, rt) -> {
-            Object return_val = Registry.RegisterCreativeModeTab(rt.Name(), modId, (ItemGroup)rg.getRawData());
-            rg.setRegistryData(return_val);
-            return return_val;
         });
     }
 
