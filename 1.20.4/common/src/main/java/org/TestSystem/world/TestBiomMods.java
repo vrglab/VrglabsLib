4e4a0eab15d2544f55d8b91078abdb5d824454a4
Vrglab <vrglab1@gmail.com>
2024-09-04 12:24:55 +0200

    1.20.4: Biome Modification registries implemented, only NeoForge fixes remain

--- 1.20.4/common/src/main/java/org/TestSystem/world/TestBiomMods.java
+++ 1.20.4/common/src/main/java/org/TestSystem/world/TestBiomMods.java
@@ -5,8 +5,10 @@ import org.TestSystem.TestMod;
 import org.TestSystem.world.Blocks.TestBlocks;
 import org.Vrglab.AutoRegisteration.Annotations.InitializableClass;
 import org.Vrglab.AutoRegisteration.Annotations.RegisterBiomeFeat;
+import org.Vrglab.AutoRegisteration.Annotations.RegisterBiomeModification;
 import org.Vrglab.AutoRegisteration.Annotations.RegisterPlacedFeat;
 import org.Vrglab.AutoRegisteration.Objects.RegistryBiomeFeat;
+import org.Vrglab.AutoRegisteration.Objects.RegistryBiomeModification;
 import org.Vrglab.AutoRegisteration.Objects.RegistryPlacedFeat;
 import org.Vrglab.Modloader.CreationHelpers.OreGenFeatCreationHelper;
 import org.Vrglab.Modloader.CreationHelpers.PlacementModifierCreationHelper;
@@ -14,7 +16,7 @@ import org.Vrglab.Modloader.Registration.Registry;
 import org.Vrglab.Modloader.enumTypes.GenReplaceables;
 import org.Vrglab.Modloader.enumTypes.VinillaBiomeTypes;
 
-@InitializableClass
+
 public class TestBiomMods {
 
     @RegisterBiomeFeat(Name = "biom_mod_con_feat_test")
@@ -24,10 +26,11 @@ public class TestBiomMods {
             );
 
     @RegisterPlacedFeat(Name = "biom_mod_placed_feat_test")
-    public static RegistryPlacedFeat PLACED_FEAT = new RegistryPlacedFeat(TestMod.MODID, PlacementModifierCreationHelper.create().HeightRangePlacement(-80, 10), CON_FEAT);
+    public static RegistryPlacedFeat PLACED_FEAT = new RegistryPlacedFeat(TestMod.MODID,
+            PlacementModifierCreationHelper.create().HeightRangePlacement(-80, 10),
+            CON_FEAT
+    );
 
-
-    public static void init() {
-        Registry.AddBiomeModification("biom_mod_test", TestMod.MODID, VinillaBiomeTypes.OVERWORLD, GenerationStep.Feature.UNDERGROUND_ORES, PLACED_FEAT.getRawData());
-    }
+    @RegisterBiomeModification(Name = "biom_mod_test")
+    public static RegistryBiomeModification BIOME_MODIFICATION = new RegistryBiomeModification(TestMod.MODID, VinillaBiomeTypes.OVERWORLD, GenerationStep.Feature.UNDERGROUND_ORES, PLACED_FEAT);
 }
---
+++ 1.20.4/common/src/main/java/org/Vrglab/AutoRegisteration/Objects/RegistryBiomeModification.java
@@ -0,0 +1,17 @@
+package org.Vrglab.AutoRegisteration.Objects;
+
+import net.minecraft.world.gen.GenerationStep;
+import org.Vrglab.Modloader.enumTypes.VinillaBiomeTypes;
+
+import java.util.HashMap;
+
+public class RegistryBiomeModification extends AutoRegisteryObject {
+
+    public RegistryBiomeModification(String modid, VinillaBiomeTypes biomeType, GenerationStep.Feature gen_step, RegistryPlacedFeat placedFeat) {
+        this.modid = modid;
+        this.args = new HashMap<>();
+        this.args.put("biome_type", biomeType);
+        this.args.put("gen_step", gen_step);
+        this.args.put("placed_feat", placedFeat);
+    }
+}
