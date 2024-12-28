c625bd678f3ee2ba304a061273c1727b854b8d0c
Vrglab <vrglab1@gmail.com>
2024-12-28 01:35:45 +0100

    1.21.4: Fixed More error's and discovered more. (Basically still WIP of porting)

--- 1.21.4/neoforge/src/main/java/org/Vrglab/neoforge/events/VillagerTradeRegisteration.java
+++ 1.21.4/neoforge/src/main/java/org/Vrglab/neoforge/events/VillagerTradeRegisteration.java
@@ -1,12 +1,13 @@
 package org.Vrglab.neoforge.events;
 
 import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.fml.common.Mod;
 import net.neoforged.neoforge.event.village.VillagerTradesEvent;
 import org.Vrglab.Utils.VLModInfo;
 import org.Vrglab.neoforge.Utils.NeoForgeRegistryCreator;
 
-@Mod.EventBusSubscriber(modid = VLModInfo.MOD_ID)
+@EventBusSubscriber(modid = VLModInfo.MOD_ID)
 public class VillagerTradeRegisteration {
     @SubscribeEvent
     public static void addCustomTrades(VillagerTradesEvent e){
