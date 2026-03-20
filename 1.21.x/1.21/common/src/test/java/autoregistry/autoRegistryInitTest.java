7c4bca5709bd4c1585335a48fa5c11aa52988d1a
Vrglab <vrglab1@gmail.com>
2026-03-20 17:40:44 +0100

    1. testing workflows unit tests

--- 1.21.x/1.21/common/src/test/java/autoregistry/autoRegistryInitTest.java
+++ 1.21.x/1.21/common/src/test/java/autoregistry/autoRegistryInitTest.java
@@ -6,10 +6,14 @@ import org.mockito.Mockito;
 import org.vrglab.vrglabsLib.api.autoRegistry.AutoRegistryLoader;
 import org.vrglab.vrglabsLib.core.VrglabsInitializer;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class autoRegistryInitTest {
 
     @Test
     void initializeAutoregistration_calls_loader() {
+        assertThat(false).isTrue();
+
         try (MockedStatic<AutoRegistryLoader> mocked = Mockito.mockStatic(AutoRegistryLoader.class)) {
             VrglabsInitializer.InitializeAutoregistration("testmod", "test.package");
 
